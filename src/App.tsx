9feed25d25796071d0cc5adbb2b7611f8850311b
Matteus <tolfx@tolfix.com>
2021-10-31 18:47:56 +0100

    Modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Admin, ListGuesser, Resource } from 'react-admin';
 import Dashboard from "./components/Dashboard";
 import authProvider from "./components/authProvider";
+import dataProvider from './components/dataProvider';
 import { CustomerList } from "./components/customers/Field";
 import { CreateCustomer } from "./components/customers/Create";
 import { EditCustomer } from "./components/customers/Edit";
@@ -10,16 +11,22 @@ import { InvoicesList } from "./components/invoices/List";
 import { EditInvoices } from "./components/invoices/Edit";
 import { CreateTransactions } from "./components/transactions/Create";
 import { ListTransactions } from "./components/transactions/List";
-import dataProvider from './components/dataProvider';
+import { CreateProducts } from './components/products/Create';
+import { CreateCategory } from './components/categories/Create';
+import { ListProducts } from './components/products/List';
+import { EditProducts } from './components/products/Edit';
+import { CreateOrders } from './components/orders/Create';
 
 function App() {
   return (
     <>
     
     <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
-        <Resource name="categories" list={ListGuesser} />
+        <Resource name="categories" list={ListGuesser} create={CreateCategory} />
         <Resource name="customers" list={CustomerList} create={CreateCustomer} edit={EditCustomer} />
         <Resource name="invoices" list={InvoicesList} show={ShowInvoices} create={CreateInvoices} edit={EditInvoices} />
+        <Resource name="products" list={ListProducts} create={CreateProducts} edit={EditProducts} />
+        <Resource name="orders" list={ListGuesser} create={CreateOrders} />
         <Resource name="transactions" list={ListTransactions} create={CreateTransactions} />
     </Admin>
     
