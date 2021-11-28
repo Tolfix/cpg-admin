9bcf26a23447d608f1ceabc550139561d9eb4197
Matteus <tolfx@tolfix.com>
2021-11-28 21:51:41 +0100

    Fixed dates

--- src/components/transactions/Create.tsx
+++ src/components/transactions/Create.tsx
@@ -21,7 +21,7 @@ export const CreateTransactions = (props: any) =>
                 </ReferenceArrayInput>
                 <NumberInput required={true} label="Amount" source="amount" />
                 <NumberInput required={true} label="Fees" source="fees" />
-                <DateInput label="Payed at" source="date" defaultValue={new Date()} />
+                <DateInput label="Payed at" source="date" defaultValue={new Date().toLocaleDateString()} />
                 <SelectInput required={true} source="payment_method" choices={[
                     { id: "none", name: "none" },
                     { id: "manual", name: "manual" },
--- src/components/transactions/Edit.tsx
+++ src/components/transactions/Edit.tsx
@@ -24,7 +24,7 @@ export const EditTrans = (props: any) =>
                 </ReferenceInput>
                 <NumberInput required={true} label="Amount" source="amount" />
                 <NumberInput required={true} label="Fees" source="fees" />
-                <DateInput label="Payed at" source="date" defaultValue={new Date()} />
+                <DateInput label="Payed at" source="date" defaultValue={new Date().toLocaleDateString()} />
                 <SelectInput required={true} source="payment_method" choices={[
                     { id: "none", name: "none" },
                     { id: "manual", name: "manual" },
